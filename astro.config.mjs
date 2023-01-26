10a33e1a608f45564ab58f7d55a9d883d31f15a3
Simeon Bain <bainsimeon@gmail.com>
2023-01-26 20:19:03 +1100

    Fix build and deploy GitHub actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://simeonbain.github.io/code-with-grace/",
+  site: "https://simeonbain.github.io",
+  base: "/code-with-grace",
   integrations: [
     tailwind({
       config: {
